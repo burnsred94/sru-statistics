1fd4220b7245ed645d62ecccc13f90a97d5cf279
Oleg Kamenshikov <burnsred94@gmail.com>
2023-05-26 12:50:38 +0300

    add init

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,19 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService: ConfigService = app.get(ConfigService);
+
+  app.setGlobalPrefix(`${configService.get('PROJECT')}`);
+
+  app.enableCors({
+
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
-    .setTitle('Search API')
-    .setDescription('Search API description')
-    .setVersion('1.0')
+    .setTitle(configService.get('SWAGGER_TITLE'))
+    .setDescription(configService.get('SWAGGER_DESCRIPTION'))
+    .setVersion(configService.get('PROJECT_VERSION'))
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  SwaggerModule.setup(`api-${configService.get('PROJECT')}`, app, document);
+  await app.listen(configService.get('PORT'));
 }
 
 bootstrap();
