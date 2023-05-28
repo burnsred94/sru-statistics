4e0b896214e0329162a2225b9438e0be6f2d95c7
Oleg Kamenshikov <burnsred94@gmail.com>
2023-05-28 12:32:44 +0300

    fix

--- api/src/modules/statistic/repositories/index.ts
+++ api/src/modules/statistic/repositories/index.ts
@@ -1,3 +1,2 @@
 export * from './articles.repository';
 export * from './keys.repository';
-export * from './statistics.repository';
--- api/src/modules/statistic/schemas/index..ts
+++ api/src/modules/statistic/schemas/index..ts
@@ -1,4 +1,2 @@
 export * from './article.schema';
 export * from './keys.schema';
-export * from './statistics.schema';
-export * from './cities.schema';
