b3e90aec454d6999296b18c98939eaac49a6cd95
Oleg Kamenshikov <burnsred94@gmail.com>
2023-08-10 07:54:46 +0300

    up time

--- api/src/modules/fetch/provider/fetch.provider.ts
+++ api/src/modules/fetch/provider/fetch.provider.ts
@@ -121,7 +121,7 @@ export class FetchProvider {
     });
   }
 
-  @Cron('50 07 * * *', { timeZone: 'Europe/Moscow' })
+  @Cron('56 07 * * *', { timeZone: 'Europe/Moscow' })
   async fetchStartUpdate() {
     await this.keysService.findAndNewPeriod();
   }
